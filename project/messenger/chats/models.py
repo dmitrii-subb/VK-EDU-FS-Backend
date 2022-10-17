844a9d0dbee0e08b9c939af86440b4110ec675ef
Dmitrii Oreshkin <71903279+dmitrii-subb@users.noreply.github.com>
2022-10-17 16:31:43 +0300

    add database migrations

--- project/messenger/chats/models.py
+++ project/messenger/chats/models.py
@@ -1,3 +1,15 @@
+from django.conf.global_settings import AUTH_USER_MODEL
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    text = models.TextField(max_length=1024)
+    date = models.DateTimeField()
+    author = models.ForeignKey(AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+
+
+class Chat(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    messages = models.ForeignKey(Message, null=True, on_delete=models.SET_NULL)
+
--- project/messenger/users/models.py
+++ project/messenger/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.conf.global_settings import AUTH_USER_MODEL
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=32, unique=True)
+    password = models.CharField(max_length=32)
